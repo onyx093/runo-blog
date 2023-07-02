64bccb62cc3014b616d58269158eb50f3f516e41
Olaleye Obidiya <olaleye.obidiya@internetbrands.com>
2023-07-03 00:59:43 +0100

    Navigation and main article on homepage are done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
+    <header class="header">
+        <div class="header__inner">
+            <h1 class="header__title">Levelup blog</h1>
+            <nav class="header__nav">
+                <ul class="header__nav-list">
+                    <li class="header__nav-item"><a class="header__nav-item-link" href="#">Home</a></li>
+                    <li class="header__nav-item"><a class="header__nav-item-link" href="#">About</a></li>
+                    <li class="header__nav-item"><a class="header__nav-item-link" href="#">Articles</a></li>
+                    <li class="header__nav-item"><a class="header__nav-item-link" href="#">Sign in</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
   <RouterView />
 </template>
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,5 +3,22 @@
 
 <template>
   <main>
+    <article class="main-article">
+        <div class="main-article__bg">
+            <img src="/images/main_bg.jpg" alt="">
+        </div>
+        <div class="main-article__inner">
+            <ul class="main-article__categories">
+                <li class="main-article__category"><a href="#" class="main-article__category-link">Adventure</a></li>
+                <li class="main-article__category"><a href="#" class="main-article__category-link">Adventure</a></li>
+            </ul>
+            <h2 class="main-article__heading">Richard Norton photorealistic rendering as real photos</h2>
+            <p class="main-article__content">
+                <time class="main-article__content--time">18:00</time>
+                <span class="main-article__content-divider"></span>
+                <span class="main-article__content-text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</span>
+            </p>
+        </div>
+    </article>
   </main>
 </template>
