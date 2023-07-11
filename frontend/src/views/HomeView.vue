887fe015e11bd023d7ac6d652ecf2898178baa73
Olaleye Obidiya <olaleye.obidiya@internetbrands.com>
2023-07-11 17:20:47 +0100

    Recreating LevelUp blog homepage

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -20,5 +20,236 @@
             </p>
         </div>
     </article>
+
+    <section class="section">
+        <div class="section__inner">
+            <h2 class="section__heading">Popular topics</h2>
+            <ul class="section__categories">
+                <li class="section__category"><a href="#" class="section__category-link section__category-link--active">All</a></li>
+                <li class="section__category"><a href="#" class="section__category-link">Adventure</a></li>
+                <li class="section__category"><a href="#" class="section__category-link">Travel</a></li>
+                <li class="section__category"><a href="#" class="section__category-link">Fashion</a></li>
+                <li class="section__category"><a href="#" class="section__category-link">Technology</a></li>
+                <li class="section__category"><a href="#" class="section__category-link">Branding</a></li>
+                <li class="section__category section__category--last"><a href="#" class="section__category-link">Branding</a></li>
+            </ul>
+
+            <div class="articles">
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/310/280" alt="" class="article__image">
+                    </div>
+                    <div class="article__content">
+                        <span class="article__date">08.08.2021</span>
+                        <h3 class="article__heading">Dream destinations to visit this year in Paris</h3>
+                        <p class="article__text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                    <div class="article__author-container">
+                        <img src="https://picsum.photos/100/100" alt="" class="article__author-image">
+                        <div class="article__author-content">
+                            <h4 class="article__author-heading">By Jennifer Lawrence</h4>
+                            <p class="article__author-role">Thinker & Designer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/310/280" alt="" class="article__image">
+                    </div>
+                    <div class="article__content">
+                        <span class="article__date">08.08.2021</span>
+                        <h3 class="article__heading">Dream destinations to visit this year in Paris</h3>
+                        <p class="article__text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                    <div class="article__author-container">
+                        <img src="https://picsum.photos/100/100" alt="" class="article__author-image">
+                        <div class="article__author-content">
+                            <h4 class="article__author-heading">By Jennifer Lawrence</h4>
+                            <p class="article__author-role">Thinker & Designer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/310/280" alt="" class="article__image">
+                    </div>
+                    <div class="article__content">
+                        <span class="article__date">08.08.2021</span>
+                        <h3 class="article__heading">Dream destinations to visit this year in Paris</h3>
+                        <p class="article__text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                    <div class="article__author-container">
+                        <img src="https://picsum.photos/100/100" alt="" class="article__author-image">
+                        <div class="article__author-content">
+                            <h4 class="article__author-heading">By Jennifer Lawrence</h4>
+                            <p class="article__author-role">Thinker & Designer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/310/280" alt="" class="article__image">
+                    </div>
+                    <div class="article__content">
+                        <span class="article__date">08.08.2021</span>
+                        <h3 class="article__heading">Dream destinations to visit this year in Paris</h3>
+                        <p class="article__text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                    <div class="article__author-container">
+                        <img src="https://picsum.photos/100/100" alt="" class="article__author-image">
+                        <div class="article__author-content">
+                            <h4 class="article__author-heading">By Jennifer Lawrence</h4>
+                            <p class="article__author-role">Thinker & Designer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/310/280" alt="" class="article__image">
+                    </div>
+                    <div class="article__content">
+                        <span class="article__date">08.08.2021</span>
+                        <h3 class="article__heading">Dream destinations to visit this year in Paris</h3>
+                        <p class="article__text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                    <div class="article__author-container">
+                        <img src="https://picsum.photos/100/100" alt="" class="article__author-image">
+                        <div class="article__author-content">
+                            <h4 class="article__author-heading">By Jennifer Lawrence</h4>
+                            <p class="article__author-role">Thinker & Designer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/310/280" alt="" class="article__image">
+                    </div>
+                    <div class="article__content">
+                        <span class="article__date">08.08.2021</span>
+                        <h3 class="article__heading">Dream destinations to visit this year in Paris</h3>
+                        <p class="article__text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                    <div class="article__author-container">
+                        <img src="https://picsum.photos/100/100" alt="" class="article__author-image">
+                        <div class="article__author-content">
+                            <h4 class="article__author-heading">By Jennifer Lawrence</h4>
+                            <p class="article__author-role">Thinker & Designer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/310/280" alt="" class="article__image">
+                    </div>
+                    <div class="article__content">
+                        <span class="article__date">08.08.2021</span>
+                        <h3 class="article__heading">Dream destinations to visit this year in Paris</h3>
+                        <p class="article__text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                    <div class="article__author-container">
+                        <img src="https://picsum.photos/100/100" alt="" class="article__author-image">
+                        <div class="article__author-content">
+                            <h4 class="article__author-heading">By Jennifer Lawrence</h4>
+                            <p class="article__author-role">Thinker & Designer</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/310/280" alt="" class="article__image">
+                    </div>
+                    <div class="article__content">
+                        <span class="article__date">08.08.2021</span>
+                        <h3 class="article__heading">Dream destinations to visit this year in Paris</h3>
+                        <p class="article__text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                    <div class="article__author-container">
+                        <img src="https://picsum.photos/100/100" alt="" class="article__author-image">
+                        <div class="article__author-content">
+                            <h4 class="article__author-heading">By Jennifer Lawrence</h4>
+                            <p class="article__author-role">Thinker & Designer</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <h2 class="section__heading">Editor's Pick</h2>
+
+            <div class="articles editors-pick">
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/420/350" alt="" class="article__image">
+                    </div>
+                    <div class="article__content article__content--bottom-border-hidden article__content--floating">
+                        <span class="article__date article__date--contrast">08.08.2021</span>
+                        <h3 class="article__heading article__heading--contrast article__heading--fixed-width">Richard Norton photorealistic rendering as real photos</h3>
+                        <p class="article__text article__text--contrast article__text--fixed-width">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                </div>
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/420/350" alt="" class="article__image">
+                    </div>
+                    <div class="article__content article__content--bottom-border-hidden article__content--floating">
+                        <span class="article__date article__date--contrast">08.08.2021</span>
+                        <h3 class="article__heading article__heading--contrast article__heading--fixed-width">Richard Norton photorealistic rendering as real photos</h3>
+                        <p class="article__text article__text--contrast article__text--fixed-width">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                </div>
+                <div class="article">
+                    <div class="article__image-container">
+                        <ul class="article__categories">
+                            <li class="article__category"><a href="#" class="article__category-link">Adventure</a></li>
+                        </ul>
+                        <img src="https://picsum.photos/420/350" alt="" class="article__image">
+                    </div>
+                    <div class="article__content article__content--bottom-border-hidden article__content--floating">
+                        <span class="article__date article__date--contrast">08.08.2021</span>
+                        <h3 class="article__heading article__heading--contrast article__heading--fixed-width">Richard Norton photorealistic rendering as real photos</h3>
+                        <p class="article__text article__text--contrast article__text--fixed-width">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productive seamless data.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
   </main>
 </template>
